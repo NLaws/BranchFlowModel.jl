ea87e946cb140edbe17286351f6fc628a47dcab7
Nick Laws <laws.nick@gmail.com>
2023-01-15 12:07:21 -0700

    copy of LinDistFlow.jl to start from
    already started adding loss terms to load balances

---
+++ src/types.jl
@@ -0,0 +1,4 @@
+abstract type AbstractInputs end
+abstract type Phases end
+abstract type SinglePhase <: Phases end
+abstract type ThreePhase <: Phases  end
---
+++ src/utils.jl
@@ -0,0 +1,110 @@
+"""
+    function i_to_j(j::String, p::Inputs)
+find all busses upstream of bus j
+
+!!! note
+    In a radial network this function should return an Array with length of 1.
+"""
+function i_to_j(j::String, p::Inputs)
+    convert(Array{String, 1}, map(x->x[1], filter(t->t[2]==j, p.edges)))
+end
+
+
+"""
+    function j_to_k(j::String, p::Inputs)
+find all busses downstream of bus j
+"""
+function j_to_k(j::String, p::Inputs)
+    convert(Array{String, 1}, map(x->x[2], filter(t->t[1]==j, p.edges)))
+end
+
+
+function rij(i::String, j::String, p::Inputs{SinglePhase})
+    linecode = get_ijlinecode(i, j, p)
+    linelength = get_ijlinelength(i, j, p)
+    rmatrix = p.Zdict[linecode]["rmatrix"] * linelength / p.Zbase
+    return rmatrix[1]
+end
+
+
+function xij(i::String, j::String, p::Inputs{SinglePhase})
+    linecode = get_ijlinecode(i, j, p)
+    linelength = get_ijlinelength(i, j, p)
+    xmatrix = p.Zdict[linecode]["xmatrix"] * linelength / p.Zbase
+    return xmatrix[1]
+end
+
+
+function get_ijlinelength(i::String, j::String, p::Inputs)
+    ij_idx = get_ij_idx(i, j, p)
+    return p.linelengths[ij_idx]
+end
+
+
+function get_ijlinecode(i::String, j::String, p::Inputs)
+    ij_idx = get_ij_idx(i, j, p)
+    return p.linecodes[ij_idx]
+end
+
+
+function get_ijedge(i::String, j::String, p::Inputs)
+    ij_idx = get_ij_idx(i, j, p)
+    return p.edges[ij_idx]
+end
+
+
+function get_ij_idx(i::String, j::String, p::Inputs)
+    ij_idxs = findall(t->(t[1]==i && t[2]==j), p.edges)
+    if length(ij_idxs) > 1
+        error("found more than one edge for i=$i and j=$j")
+    elseif length(ij_idxs) == 0
+        error("found no matching edges for i=$i and j=$j")
+    else
+        return ij_idxs[1]
+    end
+end
+
+
+function get_edge_values(var_prefix::String, m::JuMP.AbstractModel, p::Inputs)
+    vals = Float64[]
+    for edge in p.edges
+        var = string(var_prefix, "[", edge[1], "-", edge[2], "]")
+        try
+            val = value(variable_by_name(m, var))
+            if startswith(var_prefix, "l")
+                val = sqrt(val)
+            end
+            push!(vals, round(val; digits=8))
+        catch e
+            println(var, "failed", e)
+        end
+    end
+    return vals
+end
+
+
+function get_bus_values(var_prefix::String, m::JuMP.AbstractModel, p::Inputs)
+    vals = Float64[]
+    for b in p.busses
+        var = string(var_prefix,  "[", b, "]")
+        try
+            val = value(variable_by_name(m, var))
+            if startswith(var_prefix, "v")
+                val = sqrt(val)
+            end
+            push!(vals, round(val; digits=7))
+        catch e
+            println(var, " failed: ", e)
+        end
+    end
+    return vals
+end
+
+
+function get_constraints_by_variable_name(m, v::String)
+    ac = ConstraintRef[]
+    for tup in list_of_constraint_types(m)
+        append!(ac, all_constraints(m, tup[1], tup[2]))
+    end
+    filter( cr -> occursin(v, string(cr)), ac )
+end
