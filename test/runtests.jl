a3295314b310f23e176aef7281917b90800b3d13
Nick Laws <laws.nick@gmail.com>
2024-12-24 22:22:40 -0600

    mv some tests to CommonOPF where they belong

--- test/runtests.jl
+++ test/runtests.jl
@@ -113,25 +113,6 @@ end
 @testset "BranchFlowModel.jl" begin
 
 
-@testset "utilities" begin
-
-    # matrix_phases_to_vec used to build multiphase KVL
-    M = [1 2 3; 4 5 6; 7 8 9]
-
-    phases = [2, 3]
-    v = BranchFlowModel.matrix_phases_to_vec(M, phases)
-    @test v == [5, 6, 8, 9]
-
-    phases = [1]
-    v = BranchFlowModel.matrix_phases_to_vec(M, phases)
-    @test v == [1]
-
-    phases = [1, 2, 3]
-    v = BranchFlowModel.matrix_phases_to_vec(M, phases)
-    @test v == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-end
-
-
 include("test_linear.jl")
 
 
